8721da4a17d511cf41d4eab2e3533b1026de7f07
jazzerX <krikunov12@gmail.com>
2026-06-04 23:37:41 +0300

    add ogl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,55 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+
 #include <iostream>
 
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    if (!glfwInit()) {
+        std::cout << "Fatal: Faliled to initialize OpenGL" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow * window = glfwCreateWindow(800, 600, "Triangle", nullptr, nullptr);
+
+    if (!window)
+    {
+        std::cout << "Fatal: Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;        
+    }
+
+    glewExperimental = GL_TRUE;
+    if (!glewInit()) {
+        std::cout << "Fatal: Faliled to initialize GLEW" << std::endl;
+        return -1;        
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
     return 0;
 }
