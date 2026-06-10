08f26c613a53829942a8cdb43a1156bddcd9a874
jazzerX <krikunov12@gmail.com>
2026-06-10 23:25:56 +0300

    refactor: rename vertShader to shader

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,22 @@ void key_pressed(GLFWwindow * window, int key, int scancode, int action, int mod
 }
 
 GLuint CompileShader(GLuint shaderType, const char * src) {
-    GLuint vertShader = glCreateShader(shaderType);
-    glShaderSource(vertShader, 1, &src, nullptr);
-    glCompileShader(vertShader);
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
 
     constexpr auto bufSize = 512;
 
     GLint ok;
     GLchar infoLog[bufSize];
-    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &ok);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
 
     if (!ok) {
-        glGetShaderInfoLog(vertShader, bufSize, nullptr, infoLog);
+        glGetShaderInfoLog(shader, bufSize, nullptr, infoLog);
         std::cerr << "Fatal: Failed to compile shader" << std::endl << infoLog << std::endl;
     }
 
-    return vertShader;
+    return shader;
 }
 
 GLuint CreateShaderProgram(std::string_view vertFilename, std::string_view fragFilename)  {
