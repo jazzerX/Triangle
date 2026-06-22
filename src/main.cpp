37ef195abc5a3a6d488cdf56c7eb0496f9386c1e
jazzerX <krikunov12@gmail.com>
2026-06-22 21:11:23 +0300

    feat!: draw using element buffer

--- src/main.cpp
+++ src/main.cpp
@@ -105,29 +105,39 @@ int main() {
 
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
-    float vertices[] = {
+    GLfloat vertices[] = {
+        -0.5f,  0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f
     };
 
-    GLuint VBO, VAO;
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
+
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    const auto vert = fs::current_path() / "resources/shaders/triangle.vert";
-    const auto frag = fs::current_path() / "resources/shaders/triangle.frag";
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    GLuint shaderProgram = CreateShaderProgram(vert.string(), frag.string());
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
+
+    const auto vert = fs::current_path() / "resources/shaders/triangle.vert";
+    const auto frag = fs::current_path() / "resources/shaders/triangle.frag";
+    GLuint shaderProgram = CreateShaderProgram(vert.string(), frag.string());
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.114f, 0.000f, 0.231f, 1.000f);
@@ -135,13 +145,16 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
 
     return 0;
